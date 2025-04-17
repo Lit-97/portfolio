c41dcbd994d5337a28002cb4e1c14cf8bf9ccd0e
Litney Hinton <159393076+Lit-97@users.noreply.github.com>
2025-04-17 13:22:55 -0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+/*
+	Tessellate by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			wide:    [ '1281px',  '1680px' ],
+			normal:  [ '1001px',  '1280px' ],
+			narrow:  [ '737px',   '1000px' ],
+			mobile:  [ null,      '736px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+})(jQuery);
+
+var today = new Date()
+var curHr = today.getHours()
+
+if (curHr < 12) {
+    document.querySelector('#placeToSee').innerText = ('GOOD MORNING')
+} else if (curHr < 18) {
+    document.querySelector('#placeToSee').innerText = ('GOOD AFTERNOON')
+} else {
+    document.querySelector('#placeToSee').innerText = ('GOOD EVENING')
+}
